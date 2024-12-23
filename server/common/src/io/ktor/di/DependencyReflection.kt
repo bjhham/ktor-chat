287f48a367f3f4be68ab3f7cee0cbabaef10479c
Bruce Hamilton <bruce.hamilton@jetbrains.com>
2024-12-23 13:40:42 -0400

    Minor changes for DI prototype

--- server/common/src/io/ktor/di/DependencyReflection.kt
+++ server/common/src/io/ktor/di/DependencyReflection.kt
@@ -17,6 +17,13 @@ interface DependencyReflection {
         }
     }
 
+    fun <T: Any> create(kClass: KClass<T>, get: (DependencyKey) -> Any): T =
+        constructors(kClass).firstNotNullOf { constructor ->
+            constructor.callBy(constructor.parameters.associateWith { parameter ->
+                get(toKey(parameter))
+            })
+        }
+
     fun <T: Any> constructors(kClass: KClass<T>): Collection<KFunction<T>>
     fun toKey(parameter: KParameter): DependencyKey
 }
@@ -25,15 +32,7 @@ inline fun <reified T: Any> DependencyProvider.provide(kClass: KClass<out T>) =
     provide { create(kClass) }
 
 inline fun <reified T: Any> DependencyResolver.create(): T =
-    reflection.constructors(T::class).firstNotNullOf { constructor ->
-        constructor.callBy(constructor.parameters.associateWith { parameter ->
-            get(reflection.toKey(parameter))
-        })
-    }
+    reflection.create(T::class, ::get)
 
 inline fun <reified T: Any> DependencyResolver.create(kClass: KClass<out T>) =
-    reflection.constructors(kClass).firstNotNullOf { constructor ->
-        constructor.callBy(constructor.parameters.associateWith { parameter ->
-            get(reflection.toKey(parameter))
-        })
-    }
+    reflection.create(kClass, ::get)
